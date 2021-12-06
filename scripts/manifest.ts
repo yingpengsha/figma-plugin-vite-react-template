03a4ad942c42240cff0d24c8a63b896f0a326e6b
Pengsha Ying <yingfusheng@foxmail.com>
2021-12-06 20:33:31 +0800

    fix: manifest.ts generation problem

--- scripts/manifest.ts
+++ scripts/manifest.ts
@@ -1,19 +1,10 @@
 import fs from 'fs-extra'
-import chokidar from 'chokidar'
 import { genManifest } from '../src/manifest'
-import { isDev, r, log } from './utils'
+import { r, log } from './utils'
 
 export async function writeManifest () {
-  await fs.remove(r('plugin/manifest.json'))
   await fs.writeJSON(r('plugin/manifest.json'), await genManifest(), { spaces: 2 })
   log('PRE', 'write manifest.json')
 }
 
 writeManifest()
-
-if (isDev) {
-  chokidar.watch([r('src/manifest.ts'), r('package.json')])
-    .on('change', () => {
-      writeManifest()
-    })
-}
