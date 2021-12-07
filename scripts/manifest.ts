c9969a6058bef2ea47b16ace0e31c65fd268d7cf
Pengsha Ying <yingfusheng@foxmail.com>
2021-12-07 11:05:06 +0800

    fix: manifest generation problem

--- scripts/manifest.ts
+++ scripts/manifest.ts
@@ -1,8 +1,10 @@
-import fs from 'fs-extra'
+import fs, { access, mkdir } from 'fs-extra'
 import { genManifest } from '../src/manifest'
 import { r, log } from './utils'
 
 export async function writeManifest () {
+  await access(r('plugin'))
+    .catch(() => mkdir(r('plugin')))
   await fs.writeJSON(r('plugin/manifest.json'), await genManifest(), { spaces: 2 })
   log('PRE', 'write manifest.json')
 }
